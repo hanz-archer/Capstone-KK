6f60f7e7f3306a79e5ffd2b8e63193f06becff3b
Hanz <dalubatanhans@gmaal.com>
2024-03-09 15:02:51 +0800

    register auth added

--- KwentasApp/admin.py
+++ KwentasApp/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'department', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email', 'department')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'department', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- KwentasApp/views.py
+++ KwentasApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from .forms import RegistrationForm
+from django.contrib import messages
 
 def login_view(request):
     return render(request, 'KwentasApp/login.html')
@@ -8,14 +10,18 @@ def login_view(request):
 def base_view(request):
     return render(request, 'KwentasApp/base.html')
 
+
 def registration_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('base') 
+            form.save()
+            messages.success(request, 'Account created successfully. You can now log in.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Error creating account. Please check the form data.')
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
 
     return render(request, 'KwentasApp/register.html', {'form': form})
+
